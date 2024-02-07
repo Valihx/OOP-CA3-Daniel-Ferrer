6769fae6ee0abfc4d40127e1de86b618b6ba1696
Valihx <daniel.ferrerb12@gmail.com>
2024-02-07 12:25:42 +0000

    Sterted q1

--- src/CA3_Question1.java
+++ src/CA3_Question1.java
@@ -1,11 +1,17 @@
 /**
- *  Name:
- *  Class Group:
+ *  Name: Daniel Ferrer
+ *  Class Group: GD2A
  */
+import java.util.Scanner;
+import java.util.Stack;
+
 public class CA3_Question1
 {
     public static void runSimulation()
     {
+        Stack<Integer> carStack = new Stack<Integer>();
+        Stack<Integer> Parking = new Stack<Integer>();
+        Scanner scanner = new (scanner(System.in));
 
     }
 
