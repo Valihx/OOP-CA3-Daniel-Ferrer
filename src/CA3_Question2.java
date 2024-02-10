516316d44fc7c526d4e9ca0c325fb1f4936fd2eb
Valihx <daniel.ferrerb12@gmail.com>
2024-02-10 00:08:01 +0000

    finished q2

--- src/CA3_Question2.java
+++ src/CA3_Question2.java
@@ -1,3 +1,6 @@
+import org.w3c.dom.xpath.XPathResult;
+
+import java.awt.*;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -37,17 +40,57 @@ public class CA3_Question2
             System.out.println();
         }
     }
-    private static void fill(int row, int column, int[][] arr)
-    {
+    private static void fill(int row, int column, int[][] arr) {
         Stack<Pair> parameters = new Stack<>();
         parameters.push(new Pair(row,column));
         int count = 1;
+
+        while(!parameters.isEmpty()){
+            Pair current = parameters.pop();
+             row = current.row;
+             column = current.column;
+
+            if(arr[row][column] == 0){
+                arr[row][column] = count++;
+
+                //north
+                if(row > 0 && arr[row-1][column]==0){
+                    parameters.push(new Pair(row-1,column));
+                }
+                //south
+                if(row < 9 && arr[row+1][column]==0){//south
+                    parameters.push(new Pair(row+1,column));
+                }
+                //west
+                if(column > 0 && arr[row][column-1]==0){
+                    parameters.push(new Pair(row,column-1));
+                }
+                //east
+                if(column<9 && arr[row][column+1]==0){
+                    parameters.push(new Pair(row,column+1));
+                }
+
+
+
+            }
+
+        }
+
 
     }
 
     public static void start()
     {
         int[][] arr = floodFillStart();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the row");
+        int row = scanner.nextInt();
+        System.out.println("Enter the column");
+        int column = scanner.nextInt();
+
+        fill(row,column,arr);
+        display(arr);
     }
     public static void main(String[] args) {
         start();
