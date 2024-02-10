bfaa30b4a4bfe8d6f28f4f6fa9f4e899cecee6b5
Valihx <daniel.ferrerb12@gmail.com>
2024-02-10 23:59:05 +0000

    Half q3 done

--- src/CA3_Question3.java
+++ src/CA3_Question3.java
@@ -1,17 +1,39 @@
 import java.io.FileNotFoundException;
+import java.util.*;
+import java.io.File;
+
+
 /**
- *  Name:
- *  Class Group:
+ *  Name: Daniel Ferrer
+ *  Class Group: GD2A
  */
 
 public class CA3_Question3
 {
-    public static void readFile(String fileName)
+    public static void readFile(String fileName) throws FileNotFoundException
     {
+        Map<String, ArrayList<String>> identifiers = new HashMap<>();
+        Scanner scan = new Scanner(new File(fileName));
+        int lnCount=0;
 
+        while(scan.hasNextLine()){
+            lnCount++;
+            String line = scan.nextLine();
+
+            Scanner sc = new Scanner(line);
+            sc.useDelimiter("[^A-Za-z0-9_]+");
+
+            while(sc.hasNext()){
+                String identifier = sc.next();
+                if(!identifiers.containsKey(identifier)){
+                    identifiers.put(identifier,new ArrayList<>());
+                }
+                identifiers.get(identifier).add(String.valueOf(lnCount));
+            }
+        }
     }
 
     public static void main(String[] args) throws FileNotFoundException {
-        readFile("src/CA3_Question1.java");
+        readFile("CA3_Question1.java");
     }
 }
